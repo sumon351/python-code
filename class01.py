db6bf655c4b7b459a3cd9e7df2bf371dadd2f2e6
MD. NAZMUL HAQUE <68343782+sumon351@users.noreply.github.com>
2025-03-09 19:41:32 +0600

    added by me

---
+++ class01.py
@@ -0,0 +1,52 @@
+# print your name
+# print your college name
+# print your address
+# print your car name
+# print your cat name
+print('******************JOY***********************')
+print('My name is Tanvir Ahmed Joy')
+print('My college name is : DCC')
+print('Address : Mirpur -1')
+print('Car : BMW')
+print('Cat : Dora')
+print('******************JOY***********************')
+
+
+# print your name
+# print your college name
+# print your address
+# print your car name
+# print your cat name
+
+
+name='Tanvir Ahmed Joy'
+college_name='DCC'
+address='Mirpur-1'
+car_name='BMW'
+cat_name='Dora'
+print('My name is :',name)
+print('My college name is :',college_name)
+print('Address :',address)
+print('Car name :',car_name)
+print('Cat name :',cat_name)
+print('******************JOY***********************')
+
+
+
+
+# print your name
+# print your address
+# print your c_name
+# print your car name
+# print your cat name
+name ='tanvir ahmed'
+c_name ='DCC'
+address ='Mirpur-1'
+car_name ='BMW'
+cat_name ='Dora'
+
+print('my name is : ',name)
+print('my college name is : ',c_name)
+print('my location : ',address)
+print('my car name is : ',car_name)
+print('my cat name is : ',cat_name)
