17cfa42f56e00c65cfbe0421b83c9c1177da0816
MD. NAZMUL HAQUE <68343782+sumon351@users.noreply.github.com>
2025-03-09 19:41:57 +0600

    added by me

---
+++ class02.py
@@ -0,0 +1,40 @@
+# print your name
+# print your age
+# print your rice price
+
+print('**********************Joy*****************')
+name='Tanvir Ahmed Joy' # its a string value
+age=21 # its a integer value thats why we dont need a inverted comma ('' or " ")
+price=27.43 # its a floating value
+
+print('My name is :',name)
+print('My age is :',age)
+print('Rice price is :',price)
+
+# print datatype
+print(type(name))
+print(type(age))
+print(type(price))
+
+print('**********************Joy*****************')
+
+# data type
+# int ----> 10,0,-10 
+# float----> 20.33,33.12
+# string-----> 'Tanvir','21','25.44'
+# boolean-----> True/False
+# None ----> empty
+# print(type(variable))
+name='joy'
+age=21
+price=22.19
+old=False
+a=None
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(a))
+print('**********************Joy*****************')
+
+
