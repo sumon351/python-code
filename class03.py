7c050b977fe658355f8ec04de74d243c78621b99
MD. NAZMUL HAQUE <68343782+sumon351@users.noreply.github.com>
2025-03-11 19:48:56 +0600

    added by me

---
+++ class03.py
@@ -0,0 +1,37 @@
+# string and number values can operate together with *
+print('*********Joy*********')
+a,b=2,3 # number
+txt='Tanvir' #string 
+print(a*txt*b)
+print('*********Joy*********')
+
+# string and string can operate with +
+first_name='Tanvir Ahmed '
+last_name='Joy'
+name=first_name+last_name
+print(name)
+print('*********Joy*********')
+
+# numeric values can operate with all arithmetic operation
+num1=2
+num2=4
+num3=10
+result= num1+num2*num3
+print('The Result = ',result)
+print('*********Joy*********')
+
+# arithmetic expression with int and float  will result in float
+num1=2.5
+num2=2.5
+result=num1+num2 # 5.0 = float
+print(result)
+print('*********Joy*********')
+num1=20
+num2=2.5
+result=num1*num2 # 5.0 = float
+print(result)
+
+
+
+
+
